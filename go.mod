2b72a6cc9a167fbcf8a22d21aa7c8a91059326e0
dao <huangshenjun@gmail.com>
2020-06-22 23:05:50 +0800

    mod

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/DaoYoung/gen-model
+
+go 1.14
+
+require (
+	github.com/jinzhu/gorm v1.9.14
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/sirupsen/logrus v1.6.0
+	github.com/spf13/cobra v1.0.0
+	github.com/spf13/viper v1.7.0
+)
--- manager/db/mysql.go
+++ manager/db/mysql.go
@@ -1,37 +1,15 @@
 package db
+
 import (
+    "fmt"
     "github.com/jinzhu/gorm"
     _ "github.com/jinzhu/gorm/dialects/mysql"
-    "fmt"
     "github.com/spf13/viper"
-    "os"
     "log"
-    logrus "github.com/sirupsen/logrus"
-
 )
 
 var Db *gorm.DB
-// GormLogger struct
-type GormLogger struct{}
 
-// Print - Log Formatter
-func (*GormLogger) Print(v ...interface{}) {
-    switch v[0] {
-    case "sql":
-        logrus.WithFields(
-            logrus.Fields{
-                "module":        "gorm",
-                "type":          "sql",
-                "rows_returned": v[5],
-                "src":           v[1],
-                "values":        v[4],
-                "duration":      v[2],
-            },
-        ).Info(v[3])
-    case "log":
-        logrus.WithFields(logrus.Fields{"module": "gorm", "type": "log"}).Print(v[2])
-    }
-}
 func InitDb() error {
     var err error
     dsn := fmt.Sprintf(
@@ -46,16 +24,6 @@ func InitDb() error {
         log.Println("dns",dsn)
         panic(err)
     }
-    Db.SetLogger(&GormLogger{})
-    // todo log every sql
     Db.LogMode(true)
-    Formatter := new(logrus.TextFormatter)
-    logrus.SetFormatter(Formatter)
-    dir, _ := os.Getwd()
-    w, err := os.OpenFile(dir+"/logs/sql.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0640)
-    if err != nil {
-        panic(err)
-    }
-    logrus.SetOutput(w)
     return nil
 }
