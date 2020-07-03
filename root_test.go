abc6a7e6884f44a398a6ed521fe61bcf99a7e0a0
yi_dao <yi_dao@hunliji.com>
2020-07-03 10:44:04 +0800

    test

--- root_test.go
+++ root_test.go
@@ -2,34 +2,23 @@ package main
 
 import (
     "testing"
-    "os/exec"
-    "os"
-    "log"
     "fmt"
+    "github.com/DaoYoung/gen-model/cmd"
 )
+
 // func TestCreateBySelfTable(t *testing.T)  {
 //     cmd.InitConfig()
 //     cmd.CmdRequest.SetDataByViper()
 //     cmd.CmdRequest.CreateModelStruct()
 // }
-func Crasher() {
-    fmt.Println("Going down in flames!")
-    os.Exit(1)
-
+func BenchmarkHello(b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        fmt.Sprintf("hello")
+    }
 }
-func TestCrasher(t *testing.T) {
-    log.Println(1111)
-    if os.Getenv("BE_CRASHER") == "1" {
-        log.Println(1111)
-        Crasher()
-        return
-    }
-    cmd := exec.Command(os.Args[0], "-test.run=TestCrasher")
-    cmd.Env = append(os.Environ(), "BE_CRASHER=1")
-    err := cmd.Run()
-    if e, ok := err.(*exec.ExitError); ok && !e.Success() {
-        t.Log( e)
-        return
-    }
-    t.Fatalf("process ran with err %v, want exit status 1", err)
+func ExampleHello() {
+    cmd.InitConfig()
+    cmd.CmdRequest.SetDataByViper()
+    cmd.CmdRequest.CreateModelStruct()
+    // Output: hello
 }
