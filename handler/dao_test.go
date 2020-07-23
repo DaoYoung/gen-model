840a4cad0059acc2a8405829131c1c747426cc54
yidao <yi_dao@hunliji.com>
2020-07-23 18:21:18 +0800

    test

---
+++ handler/dao_test.go
@@ -0,0 +1,94 @@
+package handler
+
+import (
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/jinzhu/gorm"
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+type gromMockTest struct {
+	suite.Suite
+	Db   *gorm.DB
+	mock sqlmock.Sqlmock
+}
+
+func (s *gromMockTest) SetupSuite() {
+	db, mock, err := sqlmock.New()
+	s.mock = mock
+	require.NoError(s.T(), err)
+	s.Db, err = gorm.Open("mysql", db)
+	require.NoError(s.T(), err)
+	s.Db.LogMode(false)
+	viper.Set("is_test", true)
+	viper.Set("forceCover", true)
+}
+func (s *gromMockTest) AfterTest(_, _ string) {
+	require.NoError(s.T(), s.mock.ExpectationsWereMet())
+}
+func TestInit(t *testing.T) {
+	suite.Run(t, new(gromMockTest))
+}
+func (s *gromMockTest) TestMatchTables() {
+	var (
+		dbName    = "edu"
+		tableName = "stu%"
+	)
+	rows := sqlmock.NewRows([]string{"TABLE_SCHEMA", "TABLE_NAME"}).
+		AddRow("edu", "student").
+		AddRow("edu", "teacher")
+	s.mock.ExpectQuery("SELECT * (.+)").WithArgs(dbName, tableName).WillReturnRows(rows)
+	pluckRows := sqlmock.NewRows([]string{"TABLE_NAME"}).
+		AddRow("student")
+	s.mock.ExpectQuery("SELECT TABLE_NAME (.+)").WithArgs(dbName, tableName).WillReturnRows(pluckRows)
+	dbSchema = s.Db
+	tbs := matchTables("edu", "stu*")
+	if tbs == nil {
+		s.T().Error("not match")
+	} else {
+		assert.Equal(s.T(), "student", tbs[0], "select TABLE_NAME should be equal")
+	}
+}
+
+func (s *gromMockTest) TestGetOneTableColumns() {
+	var (
+		dbName    = "edu"
+		tableName = "student"
+	)
+	rows := sqlmock.NewRows([]string{"ColumnName", "ColumnKey", "DataType", "IsNullable", "ColumnComment"}).
+		AddRow("id", "", "int", "NO", "")
+
+	s.mock.ExpectQuery("SELECT (.+)").WithArgs(dbName, tableName).WillReturnRows(rows)
+	dbSchema = s.Db
+	tbs := getOneTableColumns("edu", "student")
+	if tbs == nil {
+		s.T().Error("not match")
+	}
+	require.NoError(s.T(), nil)
+}
+
+func (s *gromMockTest) TestFindStructMapper() {
+	var (
+		dbName     = "edu"
+		tableName  = "student"
+		structName = "studentVO"
+	)
+	rows := sqlmock.NewRows([]string{"id", "db_name", "table_name", "struct_name", "model_field_name", "model_field_type", "deleted_at"}).
+		AddRow("1", "edu", "student", "studentVO", "Id", "int", nil).
+		AddRow("2", "edu", "student", "studentVO", "RealName", "string", nil).
+		AddRow("3", "edu", "student", "studentVO", "job", "string", nil).
+		AddRow("4", "edu", "student", "studentVO", "age", "int", nil).
+		AddRow("5", "edu", "student", "studentVO", "sex", "int", nil).
+		AddRow("6", "edu", "student", "studentVO", "birthday", "*time.time", nil)
+	dbGen = s.Db
+	s.mock.ExpectQuery("SELECT (.+)").WithArgs(dbName, tableName, structName).WillReturnRows(rows)
+	mappers, err := findStructMapper("edu", "student", "studentVO")
+	if mappers == nil {
+		s.T().Error(err.Error())
+	} else {
+		assert.Equal(s.T(), len(*mappers), 6, "mappers should be 6")
+	}
+	require.NoError(s.T(), err)
+}
